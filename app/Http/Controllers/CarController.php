2d86e65f6262098a84e0dfaccc109b969e506fec
JulenZZZ <jzulaikake16dw@ikzubirimanteo.com>
2017-12-19 09:28:02 +0100

    Add files via upload

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+use Illuminate\Support\Facades\Input;
+
+use App\Cars;
+
+class CarController extends Controller
+{
+    
+
+    public function index()
+    {
+    	return view("coches");
+    }
+
+    /* 
+    public function store(Request $request)
+    {
+    	$coche = new laravel;
+    	$coche->numserie = Input::get("numserie");
+    	$coche->subject = Input::get("subject");
+    	$coche->motor = Input::get("motor");
+    	$coche->modelo = Input::get("modelo");
+    	$coche->message = Input::get("message");
+
+    	$coche->save();
+
+    	return ("los datos han sido enviados!");
+    } 
+    */
+    public function save_data(Request $request)
+    {     
+        $coche = Cars::create($request->all());
+        return redirect()->route('/inicio');
+
+    }
+
+}
