602ec89098979d34b1a454efb691b9948c2becc9
“07asier” <asier2223@gmail.com>
2017-12-30 11:47:49 +0100

    Relacion de claves extranjeras en Usuarios-Coches

--- database/migrations/2017_12_20_124733_create_coches_table.php
+++ database/migrations/2017_12_20_124733_create_coches_table.php
@@ -14,6 +14,9 @@ class CreateCochesTable extends Migration
     public function up()
     {
         Schema::create('coches', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');;
+
             $table->integer('num_serie');
             $table->string('marca');
             $table->string('motor');
@@ -31,6 +34,9 @@ class CreateCochesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('coches');
+        Schema::table('coches', function (Blueprint $table) {
+            $table->dropForeign('coches_user_id_foreign');
+        });
+
     }
 }
