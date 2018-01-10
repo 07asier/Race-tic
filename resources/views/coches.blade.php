b7fbeae239ea02f573517fb784c53ba3f092d0e2
“07asier” <asier2223@gmail.com>
2018-01-10 14:16:29 +0100

    Cambiando estilos de la vista coches

--- resources/views/coches.blade.php
+++ resources/views/coches.blade.php
@@ -13,7 +13,7 @@
             <div class="col-md-8 "> <!-- col-md-8 -->
                 <div class="">
                         <div class="row">
-                            <button type="button" data-toggle="modal" data-target="#login-modal" class="btn btn-info">Añadir</button>
+                            <button type="button" data-toggle="modal" data-target="#login-modal" class="btn btn-info">Añadir Coches</button>
                             <h3>Coches añadidos</h3>
 
                             <?php $id = Auth::user()->id;
@@ -22,14 +22,34 @@
                             ?>
 
                               @foreach($coches as $key => $data)
-                                <ul class="list-group">
-                                    <li class="list-group-item"> {{ $data->marca }} {{ $data->modelo }}</li><br></br>
 
-                                </ul>
+                            <div class="table-container">
+                                <table class="table table-filter">
+                                    <tbody>
+                                    <tr data-status="pagado">
+
+                                        <td>
+                                            <div class="media">
+                                                <a href="#" class="pull-left">
+                                                    <img src="https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg" class="media-photo">
+                                                </a>
+                                                <div class="media-body">
+
+                                                    <h4 class="title">
+                                                        {{$data->marca}}
+                                                        <span class="pull-right pagado">Apagado</span>
+                                                        <p><span class="glyphicon glyphicon-plus pull-right"></span></p>
+                                                    </h4>
+                                                    <p class="summary"> Modelo:{{$data->modelo}}</p>
+                                                    <p class="summary"> Motorización:{{$data->motor}}</p>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                             @endforeach
-
-
-
 
 
                             <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
@@ -46,14 +66,14 @@
                                                     <div class="input-group">
                                                     <span class="">
                                                     </span>
-                                                        <input type="text" class="form-control" name="numserie" id="numserie" placeholder="" required="required" /></div>
+                                                        <input type="text" class="form-control" name="numserie" id="numserie" placeholder=""  /></div>
                                                 </div>
 
                                                 <div class="form-group">
                                                     <label for="subject">
                                                         Marca</label>
-                                                    <select id="subject" name="subject" class="form-control" required="required">
-                                                        <option value="na" selected="">Escoja uno por favor:</option>
+                                                    <select id="subject" name="subject" class="form-control" >
+                                                        {{-- <option value="na" selected="">Escoja uno por favor</option>--}}
                                                         <option>AUDI</option>
                                                         <option>BMW</option>
                                                         <option>NISSAN</option>
@@ -71,7 +91,7 @@
                                                 <div class="input-group">
                                                 <span class="">
                                                 </span>
-                                                    <input type="text" class="form-control" name="motor" id="motor" placeholder="" required="required" /></div>
+                                                    <input type="text" class="form-control" name="motor" id="motor" placeholder="" /></div>
                                             </div>
 
                                             <div class="form-group">
@@ -80,21 +100,24 @@
                                                 <div class="input-group">
                                                     <span class="">
                                                     </span>
-                                                    <input type="text" class="form-control" name="modelo" id="modelo" placeholder="" required="required" /></div>
+                                                    <input type="text" class="form-control" name="modelo" id="modelo" placeholder="" /></div>
                                             </div>
 
                                             <div class="">
                                                 <div class="form-group">
                                                     <label for="name">
                                                         Mensaje</label>
-                                                    <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
+                                                    <textarea name="message" id="message" class="form-control" rows="9" cols="25"
                                                               placeholder="Mensaje"></textarea>
                                                 </div>
                                             </div>
 
                                             <button type="submit" class="btn btn-block login loginmodal-submit">
                                                 Añadir</button>
-
+                                            <br>
+                                            <div id="errordiv" class="alert alert-danger" hidden>
+                                                <strong>Error!</strong>Rellena los campos.
+                                            </div>
                                         </form>
 
                                     </div>
@@ -109,63 +132,127 @@
 
 
     <script type="text/javascript">
-/*
-        window.onload = init;
 
-        // asocia los eventos necesarios
-        function init() {
+        window.onload = iniciar;
+
+        function iniciar(){
             var formulario = document.getElementById("formulario");
             formulario.addEventListener("submit", validar);
-            formulario.elements["problema"].addEventListener("keypress", longitudMax);
+
         }
 
-        // previene escribir más caracteres de los permitidos en el campo "problema"
-        function longitudMax(event){
-            if (this.value.length === 100){
-                alert("El tamaño máximo del campo es 100.");
-                event.preventDefault();
-            }
-        }
-
-        // comprueba si el formulario está rellenado correctamente
         function validar(event){
-            var formulario = document.getElementById("formulario");
-            for (var i=0;i<formulario.elements.length;i++){
-                var elemento = formulario.elements[i];
-                if(esIncorrecto(elemento)){
-                    elemento.className = "error";
-                    event.preventDefault();
+
+            event.preventDefault();
+
+
+            var numserie = $("#numserie");
+            var marca = $("#subject");
+            var motor = $("#motor");
+            var modelo = $("#modelo");
+
+            if(numserie.val().length < 1 || motor.val().length < 1 || modelo.val().length < 1){
+
+                console.log("rellenar campos");
+                $("#errordiv").collapse();
+
+
+            }else{
+
+                var datosForm = $(this).serialize();
+
+                var request = $.ajax({
+                    dataType : "text",
+                    url:"add",
+                    type:"post",
+                    data:datosForm
+                });
+
+                request.done(function(response){
+
+                    swal({
+                        title: 'Buen Trabajo!',
+                        text: 'Coche añadido',
+                        type: 'success',
+                        confirmButtonText: 'OK',
+                    });
+
+                });
+
+                console.log("coche añadido");
+
+                var formulario = document.getElementById("#formulario");
+                for(var i=0 ; i< formulario.length; i++)
+                {
+                    formulario[i].value = "";
                 }
-                else {
-                    elemento.className = "correcto";
+
+            }
+        }
+
+
+
+
+
+
+
+        /*
+                window.onload = init;
+
+                // asocia los eventos necesarios
+                function init() {
+                    var formulario = document.getElementById("formulario");
+                    formulario.addEventListener("submit", validar);
+                    formulario.elements["problema"].addEventListener("keypress", longitudMax);
                 }
-            }
-        }
 
-        // dice si el campo es incorrecto en funcion del tipo de campo
-        function esIncorrecto(elemento){
-            if(elemento.name === "numserie" || elemento.name === "marca" || elemento.name === "motor"){
-                return estaVacio(elemento);
-            }
-            else if (elemento.name === "modelo"){
-                return telefonoIncorrecto(elemento);
-            }
-            else if (elemento.name === "mensaje"){
-                return emailIncorrecto(elemento);
-            }
-        }
+                // previene escribir más caracteres de los permitidos en el campo "problema"
+                function longitudMax(event){
+                    if (this.value.length === 100){
+                        alert("El tamaño máximo del campo es 100.");
+                        event.preventDefault();
+                    }
+                }
 
-        // comprueba si el campo está vacío
-        function estaVacio(campo){
-            if (campo.value.length === 0) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
+                // comprueba si el formulario está rellenado correctamente
+                function validar(event){
+                    var formulario = document.getElementById("formulario");
+                    for (var i=0;i<formulario.elements.length;i++){
+                        var elemento = formulario.elements[i];
+                        if(esIncorrecto(elemento)){
+                            elemento.className = "error";
+                            event.preventDefault();
+                        }
+                        else {
+                            elemento.className = "correcto";
+                        }
+                    }
+                }
 
-*/
+                // dice si el campo es incorrecto en funcion del tipo de campo
+                function esIncorrecto(elemento){
+                    if(elemento.name === "numserie" || elemento.name === "marca" || elemento.name === "motor"){
+                        return estaVacio(elemento);
+                    }
+                    else if (elemento.name === "modelo"){
+                        return telefonoIncorrecto(elemento);
+                    }
+                    else if (elemento.name === "mensaje"){
+                        return emailIncorrecto(elemento);
+                    }
+                }
+
+                // comprueba si el campo está vacío
+                function estaVacio(campo){
+                    if (campo.value.length === 0) {
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+
+        */
     </script>
 
 
--- resources/views/user/perfil.blade.php
+++ resources/views/user/perfil.blade.php
@@ -98,7 +98,6 @@
 
         }
 
-
         function validar(event){
 
             event.preventDefault();
