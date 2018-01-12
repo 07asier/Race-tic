f595da77c7999c807c24fba6a728d8f6170a65bc
JulenZZZ <jzulaikake16dw@ikzubirimanteo.com>
2018-01-12 10:52:48 +0100

    Maps añadido y informacion ampliada

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -76,9 +76,25 @@
             <div>
                 <hr class="my-4">
                 <h3 class="text-center">Quiénes somos</h3>
-                <p class="">Somos una pequeña empresa del ámbito automovilistico, que analizamos a tiempo real(a través de 4G) los datos de un coche.Mediante un <a href="https://es.wikipedia.org/wiki/OBD">OBD-II </a> almacenamos los datos en la <a href="https://es.wikipedia.org/wiki/Computaci%C3%B3n_en_la_nube" >nube</a>, y luego los plasmamos en ésta aplicación web. Es un servicio rápido, fiable y sobre todo sencillo de usar. Ofrecemos la opción de añadir más de un vehiculo a la aplicación. </p>
+                <p class="">Somos una pequeña empresa del ámbito automovilistico, que analizamos a tiempo real(a través de WiFi 4G) los datos de un coche.Mediante un dispositivo llamado <a href="https://es.wikipedia.org/wiki/OBD">OBD-II </a> el cual se puede conectar al vehiculo y se encarga de almacenar los datos en la <a href="https://es.wikipedia.org/wiki/Computaci%C3%B3n_en_la_nube" >nube</a>, y luego los plasmamos en ésta aplicación web. Es un servicio rápido, fiable y sobre todo sencillo de usar. Ofrecemos la opción de añadir más de un vehiculo a la aplicación y simple interfaz para gestionar tu perfil facil de usar. </p>
                 <hr class="my-4">
+                <h3 class="text-center">Como funciona</h3>
 
+                <h4><strong>· Paso 1</strong></h4>
+                <p>Una vez abierta la caja, encontraremos el dispositivo que tendremos que enchufar al coche</p>
+                <img src="images/obd.png" class="imgmitad" width="250" height="150">
+                <h4><strong>· Paso 2</strong></h4>
+                <p>Abrimos enchufamos el OBD en la ranura del coche. La ranura se encuentra debajo del volante.Si no encuentras dónde está la ranura busca aqui:<a href="https://blog.reparacion-vehiculos.es/conector-obd-coche"> ¿Dónde está mi ranura?</a> </p>
+                <img src="images/obd2.jpg" class="imgmitad" width="250" height="200">
+                <h4><strong>· Paso 3</strong></h4>
+                <p>Abrimos la aplicación web, insertamos nuestro coche en la seccion <a href="coches">coches</a> , introduciendo el numero de serie del OBD correspondiente, la marca del vehículo, el modelo etc ...</p>
+                <h4><strong>· Paso 4</strong></h4>
+                <p>Tutorial finalizado, ¡ ahora sólo debes usar el coche y disfrutar de todas las funciones que ofrecemos !</p>
+                <img src="images/monigote.png" class="imgmitad" width="250" height="250">
+                <hr class="my-4">
+                <h3 class="text-center">Nuestra ubicación</h3>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d725.5916790477431!2d-1.971255763124091!3d43.32751476990409!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1szubiri+manteo!5e0!3m2!1sen!2ses!4v1515747220322" id="maps" width="350" height="250" frameborder="0" style="border:0" allowfullscreen ></iframe>
+                
                 <!--<p class="text-center">Si lo prefieres, puedes seguir este <strong>videotutorial</strong> dónde se explica perfectamente cómo usarlo</p>-->
 
                 <!--<iframe class="video parallax" width="420" height="315"
