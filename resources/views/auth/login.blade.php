3ebb868b96b5e416671d212d8bc3b0097cbc7c49
“07asier” <asier2223@gmail.com>
2018-02-02 09:48:15 +0100

    Color cambiado en el panel de login y registro,

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     </div>
 
 
-<div class="container fondodiv">
+<div class="container ">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-primary">
